4aa8ef2043766cc727e84359ff609dcf702497cc
Gintare Aleknaite <gintarealeknaite@MASTUDENT-Gintare-Aleknaite-D52C5FP6YY.local>
2023-03-03 13:37:34 +0000

    TDD createNote with fetch and POST

--- notesClient.js
+++ notesClient.js
@@ -4,7 +4,20 @@ class NotesClient {
       .then((response) => response.json())
       .then((data) => callback(data));
   };
+
+  createNote(note, callback) {
+    fetch('http://localhost:3000/notes', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({"content": note}),
+    })
+    .then((response) => response.json())
+    .then((notes) => { 
+      callback(notes)
+    })
+  };
 }
 
 module.exports = NotesClient;
-
--- notesClient.test.js
+++ notesClient.test.js
@@ -3,6 +3,10 @@ const NotesClient = require('./notesClient');
 require('jest-fetch-mock').enableMocks();
 
 describe('NotesClient class', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
   it('it calls fetch and loads the data', (done) => {
     const client = new NotesClient();
 
@@ -15,4 +19,33 @@ describe('NotesClient class', () => {
       done();
     })
   });
+
+  it('checks if the correct methos is called', () => {
+    const client = new NotesClient();
+    fetch.mockResponseOnce(JSON.stringify(['1', '3']));
+    client.createNote('This is a test note', (notes) => {
+      expect(notes).toEqual(['1', '3'])
+    })
+  
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toEqual('http://localhost:3000/notes');
+      
+    const optionsObject = fetch.mock.calls[0][1];
+
+    expect(optionsObject.method).toEqual('POST');
+    expect(optionsObject.headers['Content-type']).toEqual('application/json');
+    expect(JSON.parse(optionsObject.body)).toEqual({content: 'This is a test note'});
+  })
+
+  it('creates a new note using fetch', (done) => {
+    const noteClient = new NotesClient()
+
+    fetch.mockResponseOnce(JSON.stringify(['Walk the dog']));
+
+    noteClient.createNote("Walk the dog", (notes) => {
+      expect(notes.length).toBe(1);
+      expect(notes[notes.length -1]).toEqual('Walk the dog');
+      done();
+    })
+  })
 });
